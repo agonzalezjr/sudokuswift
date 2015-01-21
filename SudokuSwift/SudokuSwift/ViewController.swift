35fc7d4a2de7f1ba75eb06696146d34d5ae3ac6e
Andy Gonzalez <agonzalezjr@gmail.com>
2015-01-20 22:33:06 -0500

    initial xcode project (no CoreData)
    Change-Id: Id961d65a428656f40b294f5f86e5c9899f276bd9

---
+++ SudokuSwift/SudokuSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuSwift
+//
+//  Created by Gonzalez, Andy on 1/20/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
