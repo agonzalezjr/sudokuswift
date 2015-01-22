f62b781207e265b3aea9fdcfd06ebf6354f912d9
Andy Gonzalez <agonzalezjr@gmail.com>
2015-01-21 23:09:34 -0500

    more work on the cell
    Change-Id: Iac4dcbe8b43cbd4267804f1333b1e35699d812f9

--- SudokuSwift/SudokuSwift/SudokuCell.swift
+++ SudokuSwift/SudokuSwift/SudokuCell.swift
@@ -8,17 +8,41 @@
 
 import Foundation
 
-class SudokuCell
+class SudokuCell : Printable, DebugPrintable
 {
 	// MARK: Properties
 	
 	// Row-Column name of the cell (e.g. A1, C7, etc...)
 	let name: String
+	private(set) var values: String
+	private(set) var peers: NSMutableSet
+	private(set) var units: Array<Array<SudokuCell>>
+	
+	// These do not seem to be working
+	var description: String {
+		return self.name
+	}
+	var debugDescription: String {
+		return self.name
+	}
 	
 	// MARK: Constructor
 	
-	init(name: String)
-	{
+	init(name: String, choices: Int = 9) {
 		self.name = name
+		self.values = "123456789"
+		self.peers = NSMutableSet()
+		self.units = []
+	}
+	
+	func isSolved() -> Bool {
+		return countElements(self.values) == 1
+	}
+	
+	func addUnit(unit: Array<SudokuCell>) {
+		self.units.append(unit)
+		for (peer) in unit {
+			self.peers.addObject(peer)
+		}
 	}
 }
--- SudokuSwift/SudokuSwiftTests/SudokuCellTests.swift
+++ SudokuSwift/SudokuSwiftTests/SudokuCellTests.swift
@@ -11,7 +11,46 @@ import XCTest
 class SudokuCellTests: XCTestCase {
 
 	func testInit() {
-		var cell = SudokuCell(name: "A1")
+		let cell = SudokuCell(name: "A1")
 		XCTAssertEqual(cell.name, "A1", "The cell name does not match")
+		XCTAssertEqual(cell.values, "123456789")
+		XCTAssertEqual(cell.peers.count, 0)
+		XCTAssertEqual(cell.units.count, 0)
+		XCTAssertFalse(cell.isSolved())
+		
+		// This isn't calling the descrition or debugDescription methods :(
+		// println(cell)
+	}
+	
+	func testValues() {
+		let cell = SudokuCell(name: "A1")
+		// TODO
+	}
+	
+	func testUnits() {
+		let cell = SudokuCell(name: "A1")
+		XCTAssertEqual(cell.units.count, 0)
+		cell.addUnit([cell, cell]);
+		XCTAssertEqual(cell.units.count, 1)
+	}
+	
+	func testPeers() {
+		let cellA1 = SudokuCell(name: "A1")
+		let cellB1 = SudokuCell(name: "B1")
+		
+		cellA1.addUnit([cellB1])
+		XCTAssertTrue(cellA1.peers.containsObject(cellB1))
+		
+		let cellB2 = SudokuCell(name: "B2")
+		let cellB3 = SudokuCell(name: "B3")
+		
+		cellA1.addUnit([cellB2, cellB3])
+		XCTAssertTrue(cellA1.peers.containsObject(cellB2))
+		XCTAssertTrue(cellA1.peers.containsObject(cellB3))
+
+		XCTAssertFalse(cellA1.peers.containsObject(cellA1))
+		XCTAssertFalse(cellB1.peers.containsObject(cellB1))
+		XCTAssertFalse(cellB2.peers.containsObject(cellB2))
+		XCTAssertFalse(cellB3.peers.containsObject(cellB3))
 	}
 }
